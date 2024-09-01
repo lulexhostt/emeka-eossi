bf99e307e9fa7f5517e21bcc66f5210b78bf0d59
lulexhostt <68097035+lulexhostt@users.noreply.github.com>
2024-09-02 00:19:33 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,53 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-export default function Home() {
+import { Sidebar, Navbar } from './components';
+import { CampaignDetails, CreateCampaign, Dashboard, Transactions, CopyTrade, ProfileDetails, Contact, Front, Dashboardd, Home2, } from './pages';
+
+const App = () => {
   return (
-    <main className="main">
-      <div className="container">
-        <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
 
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!
-          </p>
+    <>
 
-          <div className="connect">
-            <ConnectWallet />
-          </div>
-        </div>
+      {/* <Home2 /> */}
 
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
+      <div className="">
 
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
 
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
+
+
+        <div >
+          {/* <Navbar /> */}
+
+          <Routes>
+
+            <Route path="/app" element={<Front />} />
+
+            {/* <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/transactions" element={<Transactions />} /> */}
+            {/* <Route path="/copytrade" element={<CopyTrade />} /> */}
+
+            {/* <Route path="/create-campaign" element={<CreateCampaign />} />
+                    
+                    <Route path="/campaign-details/:id" element={<CampaignDetails />} /> */}
+
+            <Route path="/profile-details/:id" element={<ProfileDetails />} />
+
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+          <Front />
+          {/* <Dashboardd /> */}
+          {/* <Contact />
+                 <Dashboard />
+                 <Transactions /> */}
         </div>
       </div>
-    </main>
-  );
+
+    </>
+
+
+
+  )
 }
+
+export default App
